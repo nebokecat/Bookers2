373382e6f26275ca86fb44f55f3b1a56a6c11d66
nebokeneko <neboke.cat@gmail.com>
2020-07-02 13:20:39 +0000

    [modified] migrationファイルを整理

--- db/migrate/20200702080449_rename_book_name_column_to_books.rb
+++
@@ -1,6 +0,0 @@
-class RenameBookNameColumnToBooks < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :books, :book_name, :name
-    rename_column :books, :book_comment, :body
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_081428) do
+ActiveRecord::Schema.define(version: 2020_07_02_080250) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
